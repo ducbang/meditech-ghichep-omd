8f6a197a0354461be4f80c537cfe9471dc4018c7
Dao Huy Hoang <daohuyhoang87@gmail.com>
2018-03-18 17:14:38 +0700

    Notify to osTicket

---
+++ tools/notify-osTicket.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# osTicket Notify
+
+SECRET="24B07C94FD38DE3D2C9A0311D84353C8"
+
+if [ "$NOTIFY_WHAT" = "HOST" ]
+then
+		SUBJECT=$(echo -e "$NOTIFY_HOSTNAME - $NOTIFY_WHAT")
+        INFO=$(echo -e "$NOTIFY_HOSTNAME - $NOTIFY_WHAT: $NOTIFY_HOSTSTATE\nAt: $NOTIFY_DATE\n")
+else
+		SUBJECT=$(echo -e "$NOTIFY_HOSTNAME - $NOTIFY_WHAT: $NOTIFY_SERVICEDESC")
+        INFO=$(echo -e "HOST: $NOTIFY_HOSTNAME - $NOTIFY_WHAT: $NOTIFY_SERVICEDESC - $NOTIFY_SERVICEOUTPUT\nAt: $NOTIFY_DATE\n")
+fi
+
+
+curl -X POST -H "X-API-Key: $SECRET" -d'{"autorespond":false, "source": "API", "name": "GUEST", "email":"hoangdh@example.com", "subject":"$SUBJECT", "message":"$INFO", "topicId" : "2"}' http://192.168.30.121/helpdesk/api/http.php/tickets.json
